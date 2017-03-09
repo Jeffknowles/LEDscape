96af85b8ee38079afb0f13880f49b7521cc66713
jknowles <jeff.knowles@gmail.com>
2017-03-08 20:49:49 -0800

    autocommit

--- test.c
+++ test.c
@@ -1,8 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/time.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <netinet/in.h>
+#include <inttypes.h>
+#include <errno.h>
+#include <string.h>
+#include <math.h>
+#include <getopt.h>
+#include <ifaddrs.h>
+#include <net/if.h>
+#include <sys/mman.h>
+#include "util.h"
 #include "ledscape.h"
-#include "ledscape.c"
-#include "pru.c"
-#include "pru.h"
-int main(void){
 
-	// ledscape_init(150);
-}
+#include "lib/cesanta/net_skeleton.h"
+#include "lib/cesanta/frozen.h"
+
+#include <pthread.h>
+#include <stdbool.h>
